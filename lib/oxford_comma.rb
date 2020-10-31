7556edcd9808c3852589bc4e15b7f0bb5fe756fb
Bilal <bilalziadeh@yahoo.com>
2020-10-31 12:48:20 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size >= 3 
+        last = array.pop
+        array.push('and '+last)
+        array.join(', ')
+    elsif array.size >= 1
+        array.join(' and ')
+    else
+        array.join
+    end
 end
